6b32ecb4cf044b16735e164c99772cb476d85ccb
Giada Ramanzini <giada.ramanzini@gmail.com>
2022-08-10 19:18:28 +0200

    bozze

--- app/src/main/java/com/example/gymapp/MainActivity.kt
+++ app/src/main/java/com/example/gymapp/MainActivity.kt
@@ -1,11 +1,21 @@
 package com.example.gymapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val buttonClick = findViewById<Button>(R.id.login)
+        buttonClick.setOnClickListener {
+            val intent = Intent(this, TeamsActivity::class.java)
+            startActivity(intent)
+        }
     }
+
+
 }
